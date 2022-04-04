c7654f9de6e16cee743138b0ec6b58f42656e80d
danielgarcez78 <danielgarcez78@gmail.com>
2022-04-04 01:11:51 -0300

    Adding loading component with http interceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -9,11 +9,17 @@ import { AppRoutingModule } from '../../app/app-routing.module';
 import { MessagesComponent } from './components/messages/messages.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoadingInterceptor } from './interceptors/loading.interceptor';
+
+
 
 const COMPONENTS = [
   MessagesComponent,
   ToolbarComponent,
-  PageNotFoundComponent
+  PageNotFoundComponent,
+  LoadingComponent
 ];
 
 const MODULES = [
@@ -33,6 +39,9 @@ const MODULES = [
   exports: [
     COMPONENTS,
     MODULES
+  ],
+  providers:[
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true}
   ]
 })
 export default class CoreModule {
